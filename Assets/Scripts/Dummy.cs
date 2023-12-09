d7cf09fcb1f656bef4aec9bd69cc94535114f6ef
shettyadarsh111@gmail.com <shettyadarsh111@gmail.com>
2023-12-09 16:05:25 +0530

    devvvlop

--- Assets/Scripts/Dummy.cs
+++ Assets/Scripts/Dummy.cs
@@ -14,10 +14,6 @@ public class Dummy : MonoBehaviour
     void Update()
     {
 
-        
-        Debug.Log("hello");
-        Debug.Log("hello");
-
         Debug.Log("hello");
         Debug.Log("hello");
         Debug.Log("hello");
